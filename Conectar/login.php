e4cd49e96175e512ee91cbbcbf4e30906134401e
Daniel Garrocho <danmgap@gmail.com>
2023-07-04 18:33:59 -0300

    update para testar
    teste

--- Conectar/login.php
+++ Conectar/login.php
@@ -13,38 +13,37 @@
     $usuario_email =null;
 
     $temp_user_email = $_POST['USERemail'];
-    var_dump($temp_user_email);
     $valida_usuario = password_hash($temp_user_email, PASSWORD_DEFAULT);
     $temp_user_senha = $_POST['USERpwd'];
-    var_dump($temp_user_senha);
     $valida_senha = password_hash($temp_user_email, PASSWORD_DEFAULT);
     
     $sql1 = "SELECT id_Usuario, Email_Usuario, Senha_Usuario FROM cc_usuario WHERE Email_Usuario = '$valida_usuario'";
+    $result = mysqli_query($conn,$sql1);
     
-    
-    if($result = mysqli_query($conn,$sql1)){
+    if($result && mysqli_num_rows($result) > 0 ){
         $row = mysqli_fetch_assoc($result);
-            if(password_verify($valida_senha,$row['Senha_Usuario'])){
-                
-                $_SESSION['autenticado']='SIM';
-                $userID = $_SESSION['id'] = $row['Id_Usuario'];                           
-                $_SESSION['email'] = $_POST['USERemail'];
-                $_SESSION['senha'] = $_POST['USERpwd'];
-                $sql2 = "SELECT Primeiro_Nome_Usuario, Ultimo_Nome_Usuario FROM cc_priv_usuario WHERE id_usuario_usuario = '$userID'";
-                $resultado = mysqli_query($conn,$sql2);
-                $linha = mysqli_fetch_assoc($resultado);
-                $_SESSION['nome'] = $linha['Primeiro_Nome_Usuario'];               
-                header('Location: minhasReceitas.html');
-                
-            }else{
-                echo"<p style='color:#FF0000'>Senha inválida";
-            }
-        }
-        else{        
-            echo "<script>alert('Usuário conectado com sucesso');</script>";
-            header('Location: erro_login.php');
+        if(password_verify($valida_senha,$row['Senha_Usuario'])){
             
+            $_SESSION['autenticado']='SIM';
+            $userID = $_SESSION['id'] = $row['Id_Usuario'];                           
+            $_SESSION['email'] = $_POST['USERemail'];
+            $_SESSION['senha'] = $_POST['USERpwd'];
+            $sql2 = "SELECT Primeiro_Nome_Usuario, Ultimo_Nome_Usuario FROM cc_priv_usuario WHERE id_usuario_usuario = '$userID'";
+            $resultado = mysqli_query($conn,$sql2);
+            $linha = mysqli_fetch_assoc($resultado);
+            $_SESSION['nome'] = $linha['Primeiro_Nome_Usuario'];               
+            header('Location: minhasReceitas.html');
+            
+        }else{
+            echo"<p style='color:#FF0000'>Senha inválida";
         }
+    }
+    else{        
+        echo "<script>alert('Usuário conectado com sucesso');</script>";
+        header('Location: erro_login.php');
+        echo $temp_user_email;
+        echo $temp_user_senha;
+    }
 
 // Fecha a conexão
 mysqli_close($conn);
