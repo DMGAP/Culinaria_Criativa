0a54aeb750f4ce183c087df7d270b22c3fbdbc21
Daniel Garrocho <danmgap@gmail.com>
2023-07-04 18:56:10 -0300

    mais bagunça
    alo

--- Conectar/login.php
+++ Conectar/login.php
@@ -1,6 +1,5 @@
 <?php 
     include('conectar.php');
-    include('../Conta/conta.html').
     //variavel que verifica se a autenticação foi realizada
     $usuario_autenticado = false;
 
@@ -15,24 +14,25 @@
     $temp_user_email = $_POST['USERemail'];
     $valida_usuario = password_hash($temp_user_email, PASSWORD_DEFAULT);
     $temp_user_senha = $_POST['USERpwd'];
-    $valida_senha = password_hash($temp_user_email, PASSWORD_DEFAULT);
+    $valida_senha = password_hash($temp_user_senha, PASSWORD_DEFAULT);
     
     $sql1 = "SELECT id_Usuario, Email_Usuario, Senha_Usuario FROM cc_usuario WHERE Email_Usuario = '$valida_usuario'";
     $result = mysqli_query($conn,$sql1);
     
     if($result && mysqli_num_rows($result) > 0 ){
         $row = mysqli_fetch_assoc($result);
-        if(password_verify($valida_senha,$row['Senha_Usuario'])){
+        if(password_verify($temp_user_senha,$row['Senha_Usuario'])){
             
             $_SESSION['autenticado']='SIM';
-            $userID = $_SESSION['id'] = $row['Id_Usuario'];                           
+            $usuario_autenticado = true;
+            $usuario_id = $_SESSION['id'] = $row['Id_Usuario'];                           
             $_SESSION['email'] = $_POST['USERemail'];
             $_SESSION['senha'] = $_POST['USERpwd'];
-            $sql2 = "SELECT Primeiro_Nome_Usuario, Ultimo_Nome_Usuario FROM cc_priv_usuario WHERE id_usuario_usuario = '$userID'";
+            $sql2 = "SELECT Primeiro_Nome_Usuario, Ultimo_Nome_Usuario FROM cc_priv_usuario WHERE id_usuario_usuario = '$usuario_id'";
             $resultado = mysqli_query($conn,$sql2);
             $linha = mysqli_fetch_assoc($resultado);
             $_SESSION['nome'] = $linha['Primeiro_Nome_Usuario'];               
-            header('Location: minhasReceitas.html');
+            header('Location: ../minhasReceitas.html');
             
         }else{
             echo"<p style='color:#FF0000'>Senha inválida";
@@ -41,8 +41,7 @@
     else{        
         echo "<script>alert('Usuário conectado com sucesso');</script>";
         header('Location: erro_login.php');
-        echo $temp_user_email;
-        echo $temp_user_senha;
+        exit(0);
     }
 
 // Fecha a conexão
